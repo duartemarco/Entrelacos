66eade679c9a618d9af220d2979dff9dd8c30ec0
duartemarco <marcoduarte96sm@gmail.com>
2024-04-18 10:48:43 -0300

    Starting web project

--- src/main/java/com/entrelacos/entrelacosweb/entity/Patient.java
+++ src/main/java/com/entrelacos/entrelacosweb/entity/Patient.java
@@ -21,5 +21,35 @@ public class Patient {
         this.active = true;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
