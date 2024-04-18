5c0399c0e05e4d5fc6f915f6f34d6fbecc13b136
duartemarco <marcoduarte96sm@gmail.com>
2024-04-18 12:00:54 -0300

    fix: patient

--- src/main/java/com/entrelacos/entrelacosweb/controller/PatientController.java
+++ src/main/java/com/entrelacos/entrelacosweb/controller/PatientController.java
@@ -1,9 +1,18 @@
 package com.entrelacos.entrelacosweb.controller;
 
+import com.entrelacos.entrelacosweb.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/patient")
 public class PatientController {
+
+    @Autowired
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
 }
--- src/main/java/com/entrelacos/entrelacosweb/service/PatientService.java
+++ src/main/java/com/entrelacos/entrelacosweb/service/PatientService.java
@@ -5,6 +5,8 @@ import com.entrelacos.entrelacosweb.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PatientService {
 
@@ -14,4 +16,10 @@ public class PatientService {
     public Patient addPatient(Patient patient){
         return patientRepository.save(patient);
     }
+
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
+    }
+
+
 }
